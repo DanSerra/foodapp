a2c8e69f82fc14b510be9c8890c8cc54cf51dd09
DanSerra <serrano.r.daniela@gmail.com>
2018-01-21 19:41:45 -0600

    up things

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// st-function effect
+function loadPage() {
+  $("#nd-screen").hide(3000,
+    function (){
+    $("#nd-screen").show();
+  });
+
+}
+// nd-function effect
+  $("#st-screen").hide();
+  $("#st-screen").fadeIn(3000,
+  function(){
+    $ ("#st-screen").hide();
+  });
+//
---
+++ js/data.js
@@ -0,0 +1,15 @@
+var data {
+'vegetarian':{
+   'Pan Comido': [ ]
+   'Vegetariano Nirvana'
+   'La Pitahaya Vegana'
+   'Por Siempre Vegana Taquería'
+   'Forever Vegetariano'
+   'Los Loosers'
+   'Yug Vegetariano'
+   'Gatorta'
+   'Vegetariano Roma'
+
+
+}
+}
