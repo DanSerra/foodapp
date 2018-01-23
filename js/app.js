e47f8724d363edc92bf72d515095291582ca99d4
DanSerra <serrano.r.daniela@gmail.com>
2018-01-23 12:54:53 -0600

    actualizando

--- js/app.js
+++ js/app.js
@@ -1,15 +1,25 @@
-// st-function effect
+//st-screen
+//st-function effect
 function loadPage() {
-  $("#nd-screen").hide(3000,
+  $("#nd-screen").hide(5000,
     function (){
     $("#nd-screen").show();
   });
-
 }
-// nd-function effect
+//nd-function effect
   $("#st-screen").hide();
-  $("#st-screen").fadeIn(3000,
+  $("#st-screen").fadeIn(5000,
   function(){
     $ ("#st-screen").hide();
   });
-//
+//nd-screen
+//input
+function loadPage() {
+ $("#searcher").keyup(filterRestaurante);
+}
+//crear elemento Dom
+console.log(restaurante);
+var $newRestaurante = $("<article />", {
+ "class": "card-panel hoverable"
+});
+var $containerRestauranteName = $("<h4 />");
--- js/data.js
+++ js/data.js
@@ -1,15 +1,51 @@
-var data {
-'vegetarian':{
-   'Pan Comido': [ ]
-   'Vegetariano Nirvana'
-   'La Pitahaya Vegana'
-   'Por Siempre Vegana Taquería'
-   'Forever Vegetariano'
-   'Los Loosers'
-   'Yug Vegetariano'
-   'Gatorta'
-   'Vegetariano Roma'
-
-
-}
-}
+var data = [
+restaurante=
+   {
+   'Nombre': 'Pan Comido',
+   'Dirección': 'Tonalá 91',
+   'Rango': '$$$',
+   'Calificación': 4.3,
+   'Comida': 'Vegetariana',
+   'Imagen': '../assets/images/pancomido.jpg',
+   'Coordenadas': {
+     lng: ,
+     lat: ,
+   }
+   },
+restaurante=
+   {
+   'Nombre':'Nirvana Vegetariano'
+   'Dirección': 'Puebla 120',
+   'Rango': '$$$$',
+   'Calificaión': 5,
+   'Comida': 'Vegetariana'
+   'Imagen': '../assets/images/nirvana.jpg',
+   'Coordenadas': {
+     lng: ,
+     lat: ,
+   }
+restaurante=
+  {
+    'Nombre': 'Delirio',
+    'Dirección': 'Monterrey 116',
+    'Rango': '$$$',
+    'Calificación': 3,
+    'Comida': 'Mediterranea'
+    'Imagen': '../assets/images/delirio.png',
+    'Coordenadas': {
+      lng: ,
+      lat: ,
+  }
+restaurante=
+    {
+      'Nombre': 'El cuento ruso',
+      'Dirección': 'Gral.Benjamín Hill 99',
+      'Rango': '$$$',
+      'Calificación': 3,
+      'comida': 'Rusa',
+      'Imagen': '../assets/images/rusa.jpg',
+      'Coordenadas': {
+        lng: ,
+        lat: ,
+    }
+  ]
